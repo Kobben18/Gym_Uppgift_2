041409c859f1d426915d3b154819765868cb62a0
Jakob <jakob.b.geijer@gmail.com>
2025-10-21 15:02:00 +0200

    Initial commit

---
+++ src/Medlem.java
@@ -0,0 +1,11 @@
+import java.time.*;
+
+public class Medlem {
+    private String namn;
+    private String adress;
+    private String email;
+    private String personnummer;
+    private LocalDate medlemSedan;
+    private LocalDate betalning;
+    private String medlemTyp;
+}
